c30ead1acae04e8bbeb874d67ae0b14362ac0906
dmeskis <dmeskis@gmail.com>
2018-10-02 15:47:50 -0600

    Added to test

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
     name { Faker::Commerce.product_name }
-    description { Faker::FamousLastWords.last_word }
+    description { Faker::FamousLastWords.last_words }
     unit_price { Faker::Number.between(1, 100000) }
     merchant
   end
