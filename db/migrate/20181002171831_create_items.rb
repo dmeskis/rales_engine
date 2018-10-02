8f822f053612dd6b2701971f7b2dae8430b4b573
dmeskis <dmeskis@gmail.com>
2018-10-02 11:26:39 -0600

    Fixed migration file

--- db/migrate/20181002171831_create_items.rb
+++ db/migrate/20181002171831_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.string :name
       t.text :description
       t.bigint :unit_price
-      t.integer :merchant_id
+      t.references :merchant_id
 
       t.timestamps
     end
