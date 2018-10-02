597349c1f3e88f4ea31460288484e6a5964f5a05
dmeskis <dmeskis@gmail.com>
2018-10-02 11:48:17 -0600

    Testing

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Invoice, type: :model do
     it {should have_many(:invoice_items)}
     it {should have_many(:transactions)}
   end
+  describe 'validations' do
+    it {should validate_presence_of(:customer_id)}
+    it {should validate_presence_of(:merchant_id)}
+    it {should validate_presence_of(:status)}
+  end
 end
