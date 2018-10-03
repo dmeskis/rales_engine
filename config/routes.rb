47930bed7f65767f17f19b40d80d242c5b2fe026
dmeskis <dmeskis@gmail.com>
2018-10-03 17:52:15 -0600

    Adds endpoint for all items by merchant

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
+
+      # Merchant Endpoints
+      get 'merchants/:id/items', to: 'merchants/items#index'
       # Merchant Business Intelligence
       get 'merchants/:id/revenue', to: 'merchants/revenue#show'
       get 'merchants/:id/favorite_customer', to: 'merchants/favorite_customer#show'
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -20,7 +20,23 @@ describe 'merchants API' do
 
     expect(response).to be_successful
 
-    merchant = JSON.parse(response.body)
+    body = JSON.parse(response.body)
     expect(Merchant.count).to eq(1)
+    expect(body["name"]).to eq(Merchant.first.name)
+  end
+  it 'can show a collection of items associated with that merchant' do
+    merchant = create(:merchant)
+    item_1 = create(:item, merchant: merchant)
+    item_2 = create(:item, merchant: merchant)
+
+    get "/api/v1/merchants/#{merchant.id}/items"
+
+    expect(response).to be_successful
+
+    body = JSON.parse(response.body)
+    expect(body.first["name"]).to eq(item_1.name)
+    expect(body.first["description"]).to eq(item_1.description)
+    expect(body.last["name"]).to eq(item_2.name)
+    expect(body.last["description"]).to eq(item_2.description)
   end
 end
