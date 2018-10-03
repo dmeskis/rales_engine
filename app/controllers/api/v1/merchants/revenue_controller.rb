32a92869ae6c1d1857a092afa8f15f4b2db9fa50
dmeskis <dmeskis@gmail.com>
2018-10-03 14:40:06 -0600

    Added AM serializers

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::Merchants::RevenueController < ApplicationController
 
   def show
     merchant = Merchant.find(params[:id])
-    binding.pry
     render json: Merchant.total_revenue(merchant.id)
   end
 
