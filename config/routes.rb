fdfad6bca1d0aa0cd2270cf069059f340f6c1ba4
dmeskis <dmeskis@gmail.com>
2018-10-03 15:43:51 -0600

    Created rspec before hook for single merchant business intelligence tests

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show]
       # Merchant Business Intelligence
       get 'merchants/:id/revenue', to: 'merchants/revenue#show'
+      get 'merchants/:id/favorite_customer', to: 'merchants/favorite_customer#show'
     end
   end
 end
