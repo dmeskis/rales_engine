6e5d33d057cc38b8985123d39eb8b5bf06b05847
dmeskis <dmeskis@gmail.com>
2018-10-04 13:12:22 -0600

    Fixed item params

--- app/controllers/api/v1/items/invoice_items_controller.rb
+++ app/controllers/api/v1/items/invoice_items_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::Items::InvoiceItemsController < ApplicationController
   include ItemParams
 
   def index
+    binding.pry
     render json: Item.find(item_params[:id]).invoice_items
   end
 
