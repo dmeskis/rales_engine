11f51e7fcd019038f0073a85818b873dd6b7f055
dmeskis <dmeskis@gmail.com>
2018-10-03 18:24:02 -0600

    Created all basic endpoints for all data

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'invoice items API' do
   it 'sends a list of invoice items' do
-    create_list(:invoice_items, 3)
+    create_list(:invoice_item, 3)
 
     get '/api/v1/invoice_items'
 
