f85c43b61cabe08eebbe2a2f38ffae70104b5d3a
dmeskis <dmeskis@gmail.com>
2018-10-05 09:44:22 -0600

    Made item params pretty

--- app/controllers/concerns/item_params.rb
+++ app/controllers/concerns/item_params.rb
@@ -2,6 +2,13 @@ module ItemParams
   private
 
   def item_params
-    params.permit(:id, :quantity, :name, :description, :merchant_id, :unit_price, :updated_at, :created_at)
+    params.permit(:id,
+                  :quantity,
+                  :name,
+                  :description,
+                  :merchant_id,
+                  :unit_price,
+                  :updated_at,
+                  :created_at)
   end
 end
