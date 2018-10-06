abd5f21b18c6bafe79a292dd582891873c100b29
dmeskis <dmeskis@gmail.com>
2018-10-06 14:21:31 -0600

    Top revenue items

--- app/controllers/api/v1/items/revenue_controller.rb
+++ app/controllers/api/v1/items/revenue_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::Items::RevenueController < ApplicationController
   include ItemParams
-  
+
   def index
-    render json: Item.top_revenue_items(item_params)
+    render json: Item.top_revenue_items(quantity = item_params["quantity"].to_i)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,12 +8,12 @@ class Item < ApplicationRecord
                         :name,
                         :merchant_id
 
-  # def self.top_revenue_items(params)
-  #   select("items.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
-  #   .joins(:invoice_items, invoice_items: :invoices)
-  #   .where(transactions: {result: "success"})
-  #   .order("revenue DESC")
-  #   .group(:id)
-  #   .limit(params["quantity"])
-  # end
+  def self.top_revenue_items(quantity = 5)
+    select("items.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .joins(:invoice_items, :transactions)
+    .where(transactions: {result: "success"})
+    .order("revenue DESC")
+    .group("items.id")
+    .limit(quantity)
+  end
 end
